3112788d6c424f3c0972c61c22b58e1bd8aaa429
bmiguel- <bmiguel-@student.42lisboa.com>
2021-11-19 15:18:36 +0000

    libft submodule

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmiguel- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/19 15:00:55 by bmiguel-          #+#    #+#             */
+/*   Updated: 2021/11/19 15:17:22 by bmiguel-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef 	FT_PRINTF_H_
+# define	FT_PRINTF_H_
+
+# include <stdarg.h>
+# include "libft/libft.h"
+# include <limits.h>
+
+int	ft_printf(const char *str, ...);
+
+
+#endif
+
