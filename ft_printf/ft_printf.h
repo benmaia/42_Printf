857bac7d260dc73381d7c95049e5ca3c2bed52e0
bmiguel- <bmiguel-@student.42lisboa.com>
2021-11-19 18:09:34 +0000

    Add Makefile

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,15 +6,15 @@
 /*   By: bmiguel- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 15:00:55 by bmiguel-          #+#    #+#             */
-/*   Updated: 2021/11/19 15:17:22 by bmiguel-         ###   ########.fr       */
+/*   Updated: 2021/11/19 15:40:45 by bmiguel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef 	FT_PRINTF_H_
-# define	FT_PRINTF_H_
+#ifndef 	FT_PRINTF_H
+# define	FT_PRINTF_H
 
 # include <stdarg.h>
-# include "libft/libft.h"
+# include "libft/libft/libft.h"
 # include <limits.h>
 
 int	ft_printf(const char *str, ...);
